3515e1b190683d2e4cbdcea68e5d0728978a822c
Joseph Thomas <joseph@good-idea.studio>
2020-07-03 20:19:47 -0700

    set up reproduction

---
+++ modules/app/next.config.js
@@ -0,0 +1,9 @@
+// const path = require('path')
+// const withPlugins = require('next-compose-plugins')
+// const withTM = require('next-transpile-modules')(['@test/ui'])
+//
+// module.exports = withPlugins([withTM], {
+// 	env: {
+// 		API_URL: process.env.API_URL,
+// 	},
+// })
---
+++ modules/app/pages/path/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { Page } from '../../views/Page'
+
+export default (props) => {
+	return <Page {...props} />
+}
